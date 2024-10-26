b0080f2b7d4ca1fcb900d30ec26a60491724ea87
USER <USER@DESKTOP-KMDG42Q>
2024-10-26 19:02:37 +0300

    Proje dosyası ekle.

---
+++ Project13-IlkSınıf/Person.cs
@@ -0,0 +1,57 @@
+﻿using System; // System kütüphanesini ekleyerek temel sınıfları ve fonksiyonları kullanmamıza olanak tanır.
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project13_IlkSınıf 
+{
+    internal class Person // Sınıftaki öğrencilerin bilgilerini tutmak için sınıf oluşturuldu.
+    {
+        public string name;    // Öğrencinin adı için değişken tanımlandı.
+        public string surname; // Öğrencinin soyadı için değişken tanımlandı.
+        public DateTime birth; // Öğrencinin doğum tarihi için DateTime tipi değişken tanımlandı.
+
+        public string Name // İsim işlemleri için özellik (property).
+        {
+            get // İsim değeri alınırken kullanılacak olan get
+            {
+                return name; // Değişkenin değerini döndür.
+            }
+            set // İsim değeri atanırken kullanılacak olan set
+            {
+                name = value; // Atanan değeri değişkene kaydet.
+            }
+        }
+
+        public string Surname 
+        {
+            get 
+            {
+                return surname; 
+            }
+            set 
+            {
+                surname = value; 
+            }
+        }
+
+        public DateTime Birth 
+        {
+            get 
+            {
+                return birth; 
+            }
+            set 
+            {
+                birth = value; 
+            }
+        }
+
+        public void InformationWrite() // Verileri ekrana yazdırmak için metot.
+        {
+            // Kişinin bilgilerini konsola yazdır.
+            Console.WriteLine($"Adı: {name} / Soyadı: {surname} / Doğum Tarihi: {birth.ToString("dd/MM/yyyy")}");
+        }
+    }
+}
---
+++ Project13-IlkSınıf/Program.cs
@@ -0,0 +1,41 @@
+﻿using System; // System kütüphanesini ekleyerek temel sınıfları ve fonksiyonları kullanmamıza olanak tanır.
+
+namespace Project13_IlkSınıf 
+{
+    internal class Program // Ana program sınıfı.
+    {
+        static void Main(string[] args) 
+        {
+            // Person sınıfından nesne oluşturuyoruz.
+            Person person1 = new Person(); // İlk kişi nesnesi oluşturuluyor.
+            Person person2 = new Person(); // İkinci kişi nesnesi oluşturuluyor.
+            Person person3 = new Person(); // Üçüncü kişi nesnesi oluşturuluyor.
+            Person person4 = new Person(); // Dördüncü kişi nesnesi oluşturuluyor.
+
+            // Sınıftaki kişilerin bilgilerini oluşturuyoruz.
+            person1.Name = "Cenk"; 
+            person1.Surname = "Tosun"; 
+            person1.Birth = new DateTime(1984, 01, 01); 
+
+            person2.Name = "Hakan"; 
+            person2.Surname = "Çalhanoğlu";
+            person2.Birth = new DateTime(1985, 07, 15); 
+
+            person3.Name = "Uğurcan"; 
+            person3.Surname = "Çakır";
+            person3.Birth = new DateTime(1991, 11, 20); 
+
+            person4.Name = "Mert"; 
+            person4.Surname = "Günok"; 
+            person4.Birth = new DateTime(1990, 03, 25); 
+
+            // Her bir nesnenin bilgilerini ekrana yazdırıyoruz.
+            person1.InformationWrite();
+            person2.InformationWrite();
+            person3.InformationWrite(); 
+            person4.InformationWrite(); 
+
+            Console.ReadKey(); 
+        }
+    }
+}
